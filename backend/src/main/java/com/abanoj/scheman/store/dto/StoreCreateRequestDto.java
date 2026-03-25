3a817b91f91be719efb0f74c70896d6178d53a02
abanoj <jaabanof@gmail.com>
2026-03-25 14:07:54 +0100

    fix: correct phone regex and remove unused import in Store Dtos

--- backend/src/main/java/com/abanoj/scheman/store/dto/StoreCreateRequestDto.java
+++ backend/src/main/java/com/abanoj/scheman/store/dto/StoreCreateRequestDto.java
@@ -11,7 +11,7 @@ public record StoreCreateRequestDto(
         @Schema(description = "Store address", example = "Plaza General Mancha, N2")
         String address,
         @Schema(description = "Store phone number", example = "965910223")
-        @Pattern(regexp = "^[6-9][0-9]{9}$", message = "Must be a valid phone number")
+        @Pattern(regexp = "^[6-9][0-9]{8}$", message = "Must be a valid phone number")
         String phone,
         @Schema(description = "Is the store open 24 hours?", example = "true")
         Boolean is24h
--- backend/src/main/java/com/abanoj/scheman/store/dto/StoreUpdateRequestDto.java
+++ backend/src/main/java/com/abanoj/scheman/store/dto/StoreUpdateRequestDto.java
@@ -2,10 +2,7 @@ package com.abanoj.scheman.store.dto;
 
 import io.swagger.v3.oas.annotations.media.Schema;
 import jakarta.validation.constraints.NotBlank;
-import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Pattern;
-
-import java.util.UUID;
 
 public record StoreUpdateRequestDto(
         @Schema(description = "Store title", example = "San Blas")
@@ -14,7 +11,7 @@ public record StoreUpdateRequestDto(
         @Schema(description = "Store address", example = "Plaza General Mancha, N2")
         String address,
         @Schema(description = "Store phone number", example = "965910223")
-        @Pattern(regexp = "^[6-9][0-9]{9}$", message = "Must be a valid phone number")
+        @Pattern(regexp = "^[6-9][0-9]{8}$", message = "Must be a valid phone number")
         String phone,
         @Schema(description = "Is the store open 24 hours?", example = "true")
         Boolean is24h
