7ce90b368cb380af62b5de8d95d3befd18cf98c9
abanoj <jaabanof@gmail.com>
2026-04-11 19:04:56 +0200

    chore: update SchemanApplication configuration

--- backend/src/main/java/com/abanoj/scheman/SchemanApplication.java
+++ backend/src/main/java/com/abanoj/scheman/SchemanApplication.java
@@ -2,8 +2,10 @@ package com.abanoj.scheman;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
+@EnableScheduling
 public class SchemanApplication {
 
 	public static void main(String[] args) {
