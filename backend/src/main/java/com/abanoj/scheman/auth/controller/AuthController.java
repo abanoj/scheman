60628a3bea9ffe7d374d6bdfbd5d701f51267e36
abanoj <jaabanof@gmail.com>
2026-04-24 17:36:15 +0200

    feat(auth): add createManager endpoint to AuthController

--- backend/src/main/java/com/abanoj/scheman/auth/controller/AuthController.java
+++ backend/src/main/java/com/abanoj/scheman/auth/controller/AuthController.java
@@ -3,9 +3,11 @@ package com.abanoj.scheman.auth.controller;
 import com.abanoj.scheman.auth.dto.*;
 import com.abanoj.scheman.auth.service.AuthService;
 import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.Parameter;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.*;
@@ -26,10 +28,18 @@ public class AuthController {
         return ResponseEntity.ok(authService.login(request));
     }
 
+    @PostMapping("/signup/manager")
+    @PreAuthorize("hasRole('ADMIN')")
+    @Operation(summary = "Create a new user manager")
+    public ResponseEntity<ManagerResponseDto> createManager(@Valid @RequestBody ManagerCreateRequestDto managerCreateRequestDto){
+        return ResponseEntity.status(HttpStatus.CREATED).body(authService.createManager(managerCreateRequestDto));
+    }
+
     @PatchMapping("/{userId}/password")
     @PreAuthorize("#userId == authentication.principal.id")
     @Operation(summary = "Change the current password")
-    public ResponseEntity<Void> changePassword(UUID userId, @Valid @RequestBody ChangePasswordRequestDto changePasswordRequestDto){
+    public ResponseEntity<Void> changePassword(@Parameter(description = "User id") @PathVariable UUID userId,
+                                               @Valid @RequestBody ChangePasswordRequestDto changePasswordRequestDto){
         authService.changePassword(userId, changePasswordRequestDto);
         return ResponseEntity.noContent().build();
     }
