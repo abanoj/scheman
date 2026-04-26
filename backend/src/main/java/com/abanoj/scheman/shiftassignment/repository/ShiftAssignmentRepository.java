ad7a07999df6325360c473b4da25c61c02002e39
abanoj <jaabanof@gmail.com>
2026-04-26 17:53:38 +0200

    fix(shiftAssignment): resolve N+1 query on employee and shift

--- backend/src/main/java/com/abanoj/scheman/shiftassignment/repository/ShiftAssignmentRepository.java
+++ backend/src/main/java/com/abanoj/scheman/shiftassignment/repository/ShiftAssignmentRepository.java
@@ -3,6 +3,7 @@ package com.abanoj.scheman.shiftassignment.repository;
 import com.abanoj.scheman.shiftassignment.entity.ShiftAssignment;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
+import org.springframework.data.jpa.repository.EntityGraph;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 
@@ -12,7 +13,10 @@ import java.util.Optional;
 import java.util.UUID;
 
 public interface ShiftAssignmentRepository extends JpaRepository<ShiftAssignment, UUID> {
+    @EntityGraph(attributePaths = {"employee", "shift"})
     Page<ShiftAssignment> findAllByShiftId(Pageable pageable, UUID shiftId);
+
+    @EntityGraph(attributePaths = {"employee", "shift"})
     Page<ShiftAssignment> findAllByEmployeeId(Pageable pageable, UUID employeeId);
     Optional<ShiftAssignment> findByIdAndShiftId(UUID id, UUID shiftId);
     boolean existsByIdAndShiftId(UUID id, UUID shiftId);
