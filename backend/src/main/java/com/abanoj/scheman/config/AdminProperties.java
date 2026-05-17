92bcc344dd0d088a19bacb0d6c931baae2adc807
abanoj <jaabanof@gmail.com>
2026-05-17 18:07:29 +0200

    refactor(config): replace @Configuration with @ConfigurationPropertiesScan

--- backend/src/main/java/com/abanoj/scheman/config/AdminProperties.java
+++ backend/src/main/java/com/abanoj/scheman/config/AdminProperties.java
@@ -3,9 +3,7 @@ package com.abanoj.scheman.config;
 import lombok.Getter;
 import lombok.Setter;
 import org.springframework.boot.context.properties.ConfigurationProperties;
-import org.springframework.context.annotation.Configuration;
 
-@Configuration
 @ConfigurationProperties(prefix = "application.admin")
 @Getter
 @Setter
--- backend/src/main/java/com/abanoj/scheman/security/JwtProperties.java
+++ backend/src/main/java/com/abanoj/scheman/security/JwtProperties.java
@@ -3,9 +3,7 @@ package com.abanoj.scheman.security;
 import lombok.Getter;
 import lombok.Setter;
 import org.springframework.boot.context.properties.ConfigurationProperties;
-import org.springframework.context.annotation.Configuration;
 
-@Configuration
 @ConfigurationProperties(prefix = "application.security.jwt")
 @Getter
 @Setter
